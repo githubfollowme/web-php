6ad367f82f7987cc9088b9d3ddacf979e2d5e61b
moonfollowme <flymayday112@gmail.com>
2021-11-15 10:03:45 +0800

    測試include的功能

---
+++ 1101-1111/event.js
@@ -0,0 +1,14 @@
+const myForm=document.getElementById("myForm");
+const num1Input=document.getElementById("num1");
+const num2Input=document.getElementById("num2");
+const result
+
+myForm.addEventListener("submit",function(e){});
+e.prevent
+// num1Input.addEventListener("change",function(e){
+//     alert("changee");
+// });
+
+// num1Input.addEventListener("submit",function(e){
+// alert("55688");
+// });
---
+++ php-sql/file03.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>測試include及require</title>
+    <link rel="stylesheet" href="./style.css">
+    <!-- 這是檔案的頭 -->
+
+</head>
+<body>
+    <?php include "header.html";?>
+    <?php include "nav.php";?>
+    
+    <div class="body">
+        file03的內容
+    </div>
+    <?php include "footer.php";?>
+</body>
+</html>
+
+
